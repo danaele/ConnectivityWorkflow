247680a7a048a74c3b5c608388f99202a7bb3406
danaele <danaele@email.unc.edu>
2016-02-02 10:59:23 -0500

    Write PDF plotting : python script normalize matrix and plot with colorbar

--- plotMatrix.py
+++ plotMatrix.py
@@ -5,41 +5,46 @@ from sys import argv
 
 #args : ${SUBJECT} ${network_DIR} ${overlapName} ${loopcheck}
 
-subject = argv[1] 
-matrix = argv[2] 
-overlapName = argv[3] 
-loopcheck = argv[4] 
+subject = argv[1]
+subject_dir = argv[2] 
+matrix = argv[3] 
+overlapName = argv[4] 
+loopcheck = argv[5] 
 
 #print len(overlapName)
 #print len(loopcheck)
-fin = open(matrix,'r')
+fileMatrix = subject_dir + subject + '/Network_' + subject + overlapName + loopcheck + '/' + matrix
+fin = open(fileMatrix,'r')
 a=[]
 for line in fin.readlines():
-  print line.split('  ')
   a.append( [ float(x) for x in line.split('  ') if x != "\n" ] )   
 #print N.shape(a)
-#print a 
+print a 
 
 #Normalize NOW
 waytotal = []
-sumLine = 0
+
 j=0
 for line in a:
+  sumLine = 0
   for val in line:
     sumLine = sumLine + float(val)
     j=j+1
   waytotal.append(sumLine)
-  print j
-print len(waytotal)
-print waytotal[0]
+
+print waytotal
 i=0
 for line in a:
+  j=0
   for val in line:
-    print i
-    val = val / waytotal[i]
+    newVal = val / waytotal[i]
+    a[i][j]=newVal
+    j=j+1
   i=i+1  
-    
+  
 print a 
+    
+
 
 # plotting the correlation matrix
 fig = pl.figure(num=None, figsize=(15, 15))
